4d558dd50b8466ae0928c13e59add460aa7bffcb
hvm <aluvid073@ieselcaminass.org>
2022-11-08 20:08:32 +0100

    Initial commit

---
+++ src/Rectangulo.java
@@ -0,0 +1,14 @@
+public class Rectangulo {
+        private double lado1;
+        private double lado2;
+
+        public Rectangulo(double lado1, double lado2) {
+            lado1 = this.lado1;
+            lado2 = this.lado2;
+        }
+
+        public double obtenerArea() {
+            return lado1 * lado2;
+        }
+
+    }
